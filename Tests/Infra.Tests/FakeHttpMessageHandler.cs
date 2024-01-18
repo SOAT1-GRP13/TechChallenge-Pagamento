602d0a0ea3910590347112f14ff6d1a3486f1d6d
Christian <christiandmelo@gmail.com>
2024-01-18 12:19:23 -0300

    Testes unitários classe MercadoPagoRepository

---
+++ Tests/Infra.Tests/FakeHttpMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿public class FakeHttpMessageHandler : HttpMessageHandler
+{
+    private readonly HttpResponseMessage _fakeResponse;
+
+    public FakeHttpMessageHandler(HttpResponseMessage responseMessage)
+    {
+        _fakeResponse = responseMessage;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_fakeResponse);
+    }
+}
