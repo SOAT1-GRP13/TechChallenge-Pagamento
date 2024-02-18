b395e58979d5d03835f951604dccd31ab64ada9a
luiz-soh <luizsouza@MacBook-Pro-de-Luiz.local>
2024-02-18 17:21:27 -0300

    adicionando mais testes

---
+++ Tests/Application.Tests/Pagamentos/MercadoPago/UseCases/MercadoPagoUseCaseTests.cs
@@ -0,0 +1,79 @@
+using Application.Pagamentos.MercadoPago.Gateways;
+using Application.Pagamentos.MercadoPago.UseCases;
+using Application.Tests.Mock.UseCases;
+using Domain.MercadoPago;
+using Domain.Pedidos;
+using Domain.PedidosQR;
+using Moq;
+
+namespace Application.Tests.Pagamentos.MercadoPago.UseCases
+{
+    public class MercadoPagoUseCaseTests
+    {
+        private readonly Mock<IMercadoPagoUseCase> _useCaseMock;
+        private readonly MercadoPagoUseCase _usecase;
+
+        public MercadoPagoUseCaseTests()
+        {
+            _usecase = MockMercadoPagoUseCase.GetMercadoPagoUseCase();
+            _useCaseMock = MockMercadoPagoUseCase.GetMecadoPagoUseCaseMock();
+        }
+
+        [Fact]
+        public async void DeveRetornarString_AoChamarGerarQrCode()
+        {
+            // Arrange
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "teste", 1, 10, 10);
+            var pedido = new Pedido(Guid.NewGuid(), Guid.NewGuid(), 0, 10,
+             new List<PedidoItem>() { pedidoItem });
+            var mercadoPagoOrder = new MercadoPagoOrder(pedido);
+
+            _useCaseMock.Setup(u => u.GerarQRCode(It.IsAny<MercadoPagoOrder>())).ReturnsAsync("sucesso");
+
+            var result = await _usecase.GerarQRCode(mercadoPagoOrder);
+
+            var stringRetornado = Assert.IsType<string>(result);
+            Assert.Equal("sucesso", stringRetornado);
+
+        }
+
+        [Fact]
+        public async void DeveRetornarStatus_AoChamarPegaStatusPedido()
+        {
+            // Arrange
+            var mecadoPagoStatus = new MercadoPagoOrderStatus()
+            {
+                Id = 123,
+                Status = "closed",
+                External_reference = "external"
+            };
+
+            _useCaseMock.Setup(u => u.PegaStatusPedido(It.IsAny<long>())).ReturnsAsync(mecadoPagoStatus);
+
+            var result = await _usecase.PegaStatusPedido(123);
+
+            var statusRetornado = Assert.IsType<MercadoPagoOrderStatus>(result);
+            Assert.Equal("closed", statusRetornado.Status);
+            Assert.Equal("external", statusRetornado.External_reference);
+            Assert.Equal(123, statusRetornado.Id);
+
+        }
+
+        [Fact]
+        public async void DeveRetornarPedidoQR_AoChamarBuscaPedidoQr()
+        {
+            // Arrange
+            var qrCodeDTO = new QrCodeDTO("sucesso", "sucesso");
+
+            _useCaseMock.Setup(u => u.BuscaPedidoQr(It.IsAny<string>())).ReturnsAsync(qrCodeDTO);
+
+            var result = await _usecase.BuscaPedidoQr("123");
+
+            var qrRetornado = Assert.IsType<QrCodeDTO>(result);
+            Assert.Equal("sucesso", qrRetornado.QRData);
+            Assert.Equal("sucesso", qrRetornado.PedidoId);
+
+        }
+
+    }
+}
